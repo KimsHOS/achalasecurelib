eecccdf89fa03c4faa8dc49616b7b61a2057b8eb
kimsHOS <kims.achala@gmail.com>
2025-02-07 15:51:23 +0530

    Added precompiled achalasecurelib.framework

---
+++ achalasecurelib_build/products/library/frameworks/achalasecurelib.framework/Headers/achalasecurelib.h
@@ -0,0 +1,18 @@
+//
+//  achalasecurelib.h
+//  achalasecurelib
+//
+//  Created by EMRI on 06/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for achalasecurelib.
+FOUNDATION_EXPORT double achalasecurelibVersionNumber;
+
+//! Project version string for achalasecurelib.
+FOUNDATION_EXPORT const unsigned char achalasecurelibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <achalasecurelib/PublicHeader.h>
+
+
